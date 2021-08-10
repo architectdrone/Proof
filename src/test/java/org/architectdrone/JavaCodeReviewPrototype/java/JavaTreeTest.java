42da672be69a5e52399eb7cfa20de5070a558edd
Owen Mellema <owenmllm@gmail.com>
2021-08-09 21:53:40 -0500

    Improve testing of the JavaTreeTest

--- src/test/java/org/architectdrone/JavaCodeReviewPrototype/java/JavaTreeTest.java
+++ src/test/java/org/architectdrone/JavaCodeReviewPrototype/java/JavaTreeTest.java
@@ -2,15 +2,15 @@ package org.architectdrone.JavaCodeReviewPrototype.java;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import org.architectdrone.javacodereviewprototype.java.JavaTree;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JavaTreeTest {
     @Test
-    void itAtLeastKindOfWorks()
+    void works()
     {
         String fileA = ClassMockFactory
                 .builder()
@@ -21,8 +21,15 @@ class JavaTreeTest {
                 .build()
                 .getClassMock();
         CompilationUnit treeA = StaticJavaParser.parse(fileA);
-        JavaTree myJavaTree = new JavaTree(treeA.findRootNode(), true);
-        assertNotNull(myJavaTree);
+        JavaTree javaTree = new JavaTree(treeA.findRootNode(), true);
+        assertEquals(1, javaTree.getChildren().size());
+        assertEquals("", javaTree.getValue());
+        assertEquals(CompilationUnit.class, javaTree.getLabel());
+
+        JavaTree classJavaTree = (JavaTree) javaTree.getChildren().get(0);
+        assertEquals(3, classJavaTree.getChildren().size());
+        assertEquals("", classJavaTree.getValue());
+        assertEquals(ClassOrInterfaceDeclaration.class, classJavaTree.getLabel());
 
     }
 }

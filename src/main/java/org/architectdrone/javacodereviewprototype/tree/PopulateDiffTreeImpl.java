4127dc1f3cb586884d88db1b5d6a0f40a513fb17
Owen Mellema <owen.mellema@cerner.com>
2021-08-24 18:09:17 -0500

    Move methods that are only needed in PopulateDiffTreeImpl out of DiffTree

--- src/main/java/org/architectdrone/javacodereviewprototype/tree/PopulateDiffTreeImpl.java
+++ src/main/java/org/architectdrone/javacodereviewprototype/tree/PopulateDiffTreeImpl.java
@@ -8,10 +8,10 @@ import java.util.function.BiConsumer;
 import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
-import static org.architectdrone.javacodereviewprototype.tree.DiffTree.areNodesMisaligned;
-import static org.architectdrone.javacodereviewprototype.tree.DiffTree.getMisalignedDual;
-import static org.architectdrone.javacodereviewprototype.tree.DiffTree.getNodeGeneric;
+import static org.architectdrone.javacodereviewprototype.tree.ReferenceType.MOVE_TO;
+import static org.architectdrone.javacodereviewprototype.tree.ReferenceType.NONE;
 
 /**
  * Populates a diff tree. The results are:
@@ -55,7 +55,7 @@ public class PopulateDiffTreeImpl implements PopulateDiffTree {
                 }
 
                 //Identify and fix mismatched nodes
-                doForEachPairOfMatchedNodes(parent, PopulateDiffTreeImpl::handleMisalignedNodes, DiffTree::areNodesMisaligned);
+                doForEachPairOfMatchedNodes(parent, PopulateDiffTreeImpl::handleMisalignedNodes, PopulateDiffTreeImpl::areNodesMisaligned);
 
                 //First, nodes that are created and nodes that are moved into this tree
                 if (parent.isMatched())
@@ -67,7 +67,7 @@ public class PopulateDiffTreeImpl implements PopulateDiffTree {
                 doForEachChild(parent, PopulateDiffTreeImpl::deleteNode, a -> (!a.isMatched() && (a.getReferenceType() == ReferenceType.NONE)));
             }
         }
-        treeA.rectifyNodes();
+        rectifyNodes(treeA);
     }
 
     private static <L> void deleteNode(DiffTree<L> unmatchedNode)
@@ -136,9 +136,9 @@ public class PopulateDiffTreeImpl implements PopulateDiffTree {
         createdNode.setMatch(original);
         createdNode.setReferenceType(referenceType);
 
-        DiffTree<L> previous = getNodeGeneric(original, 1, DiffTree::getPreviousMatched, a -> true);
+        DiffTree<L> previous = getNodeGeneric(original, 1, PopulateDiffTreeImpl::getPreviousMatched, a -> true);
         DiffTree<L> previousMatch = previous != null ? previous.getMatch() : null;
-        createdNode.getParent().insertNodeAfter(previousMatch, createdNode);
+        insertNodeAfter(createdNode.getParent(), previousMatch, createdNode);
 
         return createdNode;
     }
@@ -162,7 +162,7 @@ public class PopulateDiffTreeImpl implements PopulateDiffTree {
         moveFromNode.setHasAdvancedDataBeenPopulated(true);
         moveFromNode.setReferenceType(ReferenceType.MOVE_FROM);
 
-        current.getParent().insertNodeAfter(beforeNewNode, moveFromNode);
+        insertNodeAfter(current.getParent(), beforeNewNode, moveFromNode);
         maximumMisalignedNode.getMatch().unmatch();
         moveFromNode.setMatch(maximumMisalignedNode.getMatch());
         maximumMisalignedNode.unmatch();
@@ -186,16 +186,200 @@ public class PopulateDiffTreeImpl implements PopulateDiffTree {
 
     private static <L> void doForEachPairOfMatchedNodes(DiffTree<L> parent, BiConsumer<DiffTree<L>, DiffTree<L>> handler, BiPredicate<DiffTree<L>, DiffTree<L>> discriminator)
     {
-        DiffTree<L> current = getNodeGeneric(parent.getFirst(), 0, DiffTree::getNextMatched, a -> a.getReferenceType() == ReferenceType.NONE && a.isMatched());
+        DiffTree<L> current = getNodeGeneric(parent.getFirst(), 0, PopulateDiffTreeImpl::getNextMatched, a -> a.getReferenceType() == ReferenceType.NONE && a.isMatched());
         while (true) {
-            DiffTree<L> next = getNodeGeneric(current, 1, DiffTree::getNextMatched, a -> a.getReferenceType() == ReferenceType.NONE);
+            DiffTree<L> next = getNodeGeneric(current, 1, PopulateDiffTreeImpl::getNextMatched, a -> a.getReferenceType() == ReferenceType.NONE);
             if (next == null) {
                 break;
             }
             if (discriminator.test(current, next)) {
                 handler.accept(current, next);
             }
-            current = getNodeGeneric(current, 1, DiffTree::getNextMatched, a -> a.getReferenceType() == ReferenceType.NONE);
+            current = getNodeGeneric(current, 1, PopulateDiffTreeImpl::getNextMatched, a -> a.getReferenceType() == ReferenceType.NONE);
+        }
+    }
+
+    /**
+     * Ensures nodes are in correct form after running PopulateDiffTree.
+     */
+    private static <L> void rectifyNodes(DiffTree<L> toRectify)
+    {
+        DiffTree<L> current = toRectify.getFirst();
+        int childNumber = -1;
+        while (true)
+        {
+            if (current == null)
+            {
+                break;
+            }
+            if (current.getPrevious() == null || current.getPrevious() != null && current.getReferenceType().linesCreated + current.getPrevious()
+                    .getReferenceType().linesCreated != 0 || current.getReferenceType() == NONE) {
+                childNumber++;
+            }
+            if (current.getReferenceType() == MOVE_TO)
+            {
+                current.unmatch();
+            }
+            current.setChildNumber(childNumber);
+            rectifyNodes(current);
+            current = current.getNext();
+        }
+    }
+
+    /**
+     * Inserts a node after some other node.
+     * Should be called on the parent of before
+     * @param before The node before the new node
+     * @param newNode The new node
+     */
+    private static <L> void insertNodeAfter(DiffTree<L> parent, DiffTree<L> before, DiffTree<L> newNode)
+    {
+        if (before == null)
+        {
+            newNode.setNext(parent.getFirst());
+            if (parent.getFirst() != null)
+            {
+                parent.getFirst().setPrevious(newNode);
+            }
+            parent.setFirst(newNode);
+        }
+        else
+        {
+            DiffTree<L> oldAfter = before.getNext();
+            before.setNext(newNode);
+            newNode.setNext(oldAfter);
+            newNode.setPrevious(before);
+            if (oldAfter != null)
+            {
+                oldAfter.setPrevious(newNode);
+            }
+        }
+
+        parent.getChildren().add(newNode);
+    }
+
+    /**
+     * @return the next matched node after this ndoe, or null.
+     */
+    private static <L> DiffTree<L> getNextMatched(DiffTree<L> current)
+    {
+        return getNodeGeneric(current.getNext(), 0, DiffTree::getNext, a -> a.isMatched() && a.getParent() == current.getParent());
+    }
+
+    /**
+     * @return the next matched NONE node after this ndoe, or null.
+     */
+    private static <L> DiffTree<L> getNextMatchedNone(DiffTree<L> current)
+    {
+        return getNodeGeneric(current.getNext(), 0, DiffTree::getNext, a -> a.isMatched() && a.getParent() == current.getParent() && a.getReferenceType() == NONE);
+    }
+
+    /**
+     * @return the previous matched NONE after this node
+     */
+    private static <L> DiffTree<L> getPreviousMatchedNone(DiffTree<L> current)
+    {
+        return getNodeGeneric(current.getPrevious(), 0, DiffTree::getPrevious, a -> a.isMatched() && a.getParent() == current.getParent()&& a.getReferenceType() == NONE);
+    }
+
+    /**
+     * @return the previous matched after this node
+     */
+    private static <L> DiffTree<L> getPreviousMatched(DiffTree<L> current)
+    {
+        return getNodeGeneric(current.getPrevious(), 0, DiffTree::getPrevious, a -> a.isMatched() && a.getParent() == current.getParent());
+    }
+
+    /**
+     * Gets a node by some criteria
+     * @param start The first node in the sequence
+     * @param index Which node to get.
+     * @param iterator The function to return the next node
+     * @param discriminator The function to determine if it valid
+     * @param <L> The label type
+     * @return The node
+     */
+    private static <L> DiffTree<L> getNodeGeneric(DiffTree<L> start,  int index, UnaryOperator<DiffTree<L>> iterator, Predicate<DiffTree<L>> discriminator)
+    {
+        int currentIndex = 0;
+        DiffTree<L> current = start;
+        while (true)
+        {
+            if (current == null)
+            {
+                return null;
+            }
+            if (discriminator.test(current))
+            {
+                if (currentIndex == index)
+                {
+                    return current;
+                }
+                currentIndex++;
+            }
+            current = iterator.apply(current);
+        }
+    }
+
+    /**
+     * Determines whether two nodes are misaligned.
+     * @param x The first node.
+     * @param y The second node.
+     * @param <L> The label type.
+     * @return Whether they are misaligned.
+     */
+    static <L> boolean areNodesMisaligned(DiffTree<L> x, DiffTree<L> y)
+    {
+        if (x == null || y == null)
+        {
+            return false;
+        }
+
+        boolean xThenY = getNextMatchedNone(x) == y;
+        DiffTree<L> xNextMatch = getNextMatchedNone(x.getMatch());
+        DiffTree<L> xPrevMatch = getPreviousMatchedNone(x.getMatch());
+
+        while (true)
+        {
+            if (xPrevMatch == y.getMatch())
+            {
+                return xThenY;
+            }
+            else if (xNextMatch == y.getMatch())
+            {
+                return !xThenY;
+            }
+            else if (xNextMatch == null && xPrevMatch == null)
+            {
+                return false;
+            }
+            else {
+                xNextMatch = xNextMatch != null ? getNextMatchedNone(xNextMatch) : null;
+                xPrevMatch = xPrevMatch != null ? getPreviousMatchedNone(xPrevMatch) : null;
+            }
+        }
+    }
+
+    /**
+     * Gets the misaligned dual of x
+     * @param x The node to get the dual of
+     * @param <L> The label type
+     * @return The misaligned dual, or null if x is not misaligned.
+     */
+    static <L> DiffTree<L> getMisalignedDual(DiffTree<L> x)
+    {
+        DiffTree<L> xMatch = x.getMatch();
+        DiffTree<L> preX = getPreviousMatched(xMatch);
+        DiffTree<L> postX = getNextMatched(xMatch);
+
+        if (areNodesMisaligned(xMatch, preX))
+        {
+            return preX;
+        } else if (areNodesMisaligned(xMatch, postX))
+        {
+            return postX;
+        } else {
+            return null;
         }
     }
 }

2f01e64a6dd2e7d5254a10fd7a31f15148efd167
Owen Mellema <owen.mellema@cerner.com>
2021-11-09 20:35:42 -0600

    Setup two new classes

---
+++ service-impl/src/main/java/org/architectdrone/javacodereviewprototype/context/language/unparser/Discriminator.java
@@ -0,0 +1,14 @@
+package org.architectdrone.javacodereviewprototype.context.language.unparser;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/*
+ * Returns the correct pattern to use.
+ * <p>
+ * Copyrights 2021. Cerner Corporation.
+ * @author Pharmacy Outpatient
+ */
+abstract class Discriminator<T> {
+    void Pattern
+}
---
+++ service-impl/src/main/java/org/architectdrone/javacodereviewprototype/context/language/unparser/UnparserPattern.java
@@ -0,0 +1,14 @@
+package org.architectdrone.javacodereviewprototype.context.language.unparser;
+
+import java.util.List;
+import org.architectdrone.javacodereviewprototype.context.language.display.DisplayElement;
+
+/*
+ * Takes a node and unparses it in a certain way
+ * <p>
+ * Copyrights 2021. Cerner Corporation.
+ * @author Pharmacy Outpatient
+ */
+abstract class UnparserPattern {
+    abstract List<DisplayElement> unparse(String label, List<DisplayElement> displayElements);
+}

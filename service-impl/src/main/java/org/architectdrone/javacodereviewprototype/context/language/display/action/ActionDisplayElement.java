9ce72db35eea3c9f7f0a64e7532f64a43e46d052
Owen Mellema <owenmllm@gmail.com>
2021-11-28 12:21:51 -0600

    Create actions

---
+++ service-impl/src/main/java/org/architectdrone/javacodereviewprototype/context/language/display/action/ActionDisplayElement.java
@@ -0,0 +1,15 @@
+package org.architectdrone.javacodereviewprototype.context.language.display.action;
+
+import org.architectdrone.javacodereviewprototype.context.language.display.DisplayElement;
+import org.architectdrone.javacodereviewprototype.context.language.display.MetaDisplayElement;
+
+import java.util.List;
+
+/**
+ * Represents a {@link org.architectdrone.javacodereviewprototype.tree.ReferenceType}
+ */
+public class ActionDisplayElement extends MetaDisplayElement {
+    public ActionDisplayElement(List<DisplayElement> children) {
+        super(children);
+    }
+}
